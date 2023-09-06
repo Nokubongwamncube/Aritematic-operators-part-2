a25cf926154da0099d8812c0a14a0bb562d1fda7
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-06 11:15:27 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        Dim dprice As Decimal
+        Dim dquantity As Decimal
+        Dim dDiscount As Decimal
+        Dim dVAT As Decimal
+
+        Dim dTotalcostVAT As Decimal
+        Dim DTotalcostexVAT As Decimal
+
+        dprice = 10
+        dquantity = 10
+        dDiscount = 2
+        dVAT = 0.15
+
+
+        DTotalcostexVAT = dquantity * (dprice - dDiscount)
+        MsgBox("total cost exclusive VAT" & DTotalcostexVAT)
+
+        dTotalcostVAT = DTotalcostexVAT * dVAT + DTotalcostexVAT
+
+
+        MsgBox("total cost inclusive VAT" & dTotalcostVAT)
+
+
+
+    End Sub
+End Class
